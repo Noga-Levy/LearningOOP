1a5a05c5d8cc1b1185f387eedbf71888694bbfd4
Noga Levy <nogalevy17@gmail.com>
2025-08-14 12:30:01 -0700

    Created Lesson4.py, and edited the description of Lesson3.py.

---
+++ Lesson4.py
@@ -0,0 +1,8 @@
+"""
+Written on 14 of August 2025, by Noga Levy.
+
+This program is 4 out of 6 programs I made using Corey Schafer's (a YouTube channel) tutorials. This is the link to
+the video used for this program:
+https://www.youtube.com/watch?v=RSl87lqOXDE&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc&index=4
+All about inheritance and creating subclasses
+"""
