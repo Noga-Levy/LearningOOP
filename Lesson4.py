38551bff20ed4e9fec4e8cee1d854d13106d80db
Noga Levy <nogalevy17@gmail.com>
2025-08-19 12:22:47 -0700

    Edited Lesson4.py, created Lesson5.py and added description

--- Lesson4.py
+++ Lesson4.py
@@ -1,5 +1,5 @@
 """
-Written on 14 of August 2025, by Noga Levy.
+Written on 14-18 of August 2025, by Noga Levy.
 
 This program is 4 out of 6 programs I made using Corey Schafer's (a YouTube channel) tutorials. This is the link to
 the video used for this program:
---
+++ Lesson5.py
@@ -0,0 +1,8 @@
+"""
+Written on 19 of August 2025, by Noga Levy.
+
+This program is 5 out of 6 programs I made using Corey Schafer's (a YouTube channel) tutorials. This is the link to
+the video used for this program:
+https://www.youtube.com/watch?v=3ohzBxoFHAY&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc&index=5
+All about magic/dunder methods
+"""
